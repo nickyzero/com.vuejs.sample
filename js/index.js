b98c66f34bf132f79a117d53c711100e955ccb97
Jaeduck Oh <jaeduck.oh@gmail.com>
2020-03-16 14:42:57 +0900

    Initial Release

---
+++ js/index.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        message : "Hello webOS OpenSourceEdtion World! - Vue.js",
+        timezone : "",
+        date: ""
+    },
+    methods:{
+        setLocalTime: function(args){
+            this.date = args.year + " / " + args.month + " / " + args.day;
+        },
+        setTimeZone: function(args){
+            this.timezone = args;
+        }
+    }
+});
+
+//sample code for calling LS2 API
+var lunaReq= webOS.service.request("luna://com.webos.service.systemservice",
+{
+     method:"time/getSystemTime",
+     parameters:{},
+     onSuccess: function (args) {
+         console.log("UTC:", args.utc);
+         app.setLocalTime(args.localtime);
+         app.setTimeZone(args.timezone);
+     },
+     onFailure: function (args) {
+         app.setLocalTime(args.errorText);
+         app.setTimeZone(args.errorText);
+     }
+ });
